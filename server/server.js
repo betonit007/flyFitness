3d3f3346f8a5c2b50c73d550379de5f3011b5f6c
betonit007 <timnagorski@hotmail.com>
2020-12-01 10:49:29 -0500

    changing server settings to solve upload issue

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ connectDB()
 
 const app = express()
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
@@ -31,14 +31,14 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
 //app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) // image not found on front end when using this line (below works)
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 //Sends back custom error message including requested route that was not found
 app.use(notFound)
