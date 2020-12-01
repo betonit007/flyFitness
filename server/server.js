3b8e48ab80a40ff3daf3dc2e7418ed7a6aa8aaf3
betonit007 <timnagorski@hotmail.com>
2020-12-01 11:02:42 -0500

    debugging image upload

--- server/server.js
+++ server/server.js
@@ -31,14 +31,14 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) // image not found on front end when using this line (below works)
+app.use('/uploads', express.static('uploads'))
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
-
-//app.use('/uploads', express.static(path.join(__dirname, 'uploads'))) // image not found on front end when using this line (below works)
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 //Sends back custom error message including requested route that was not found
 app.use(notFound)
